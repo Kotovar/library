0613cefbbdf64ad95bc3e6cfc02ee19d134a5b0b
Kotovar <77914431+Kotovar@users.noreply.github.com>
2023-09-14 00:30:26 +0700

    add html filling, add css styles, start js code

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
-// function Book(title, author, pages, read) {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.read = read;
-//   this.info = function () {
-//     return `${title} by ${author}, ${pages}, ${read} yet`;
-//   };
-// }
+//Переменные
+const myLibrary = [];
+const openButton = document.getElementById("add");
+const dialog = document.getElementById("bookDialog");
+const form = dialog.querySelector("form");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
 
-// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read");
-// console.log(theHobbit.info());
-
-class Book {
-  constructor(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
-
-  info() {
-    return `${this.title} by ${this.author}, ${this.pages}, ${this.read} yet`;
-  }
+// функция конструктор для добавления книг
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read");
-console.log(theHobbit.info());
+// функция создания книги с помощью конструктора и добавление в массив
+function createBook(title, author, pages, read) {
+  let book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
+
+createBook("The Hobbit", "J.R.R. Tolkien", 295, false);
+createBook("The bit", "author", 100, true);
+
+// Открываем диалог при нажатии на кнопку
+openButton.addEventListener("click", function () {
+  reset();
+  dialog.showModal();
+});
+
+// Закрываем диалог при клике вне диалога
+dialog.addEventListener("click", function (e) {
+  if (e.target === dialog) {
+    dialog.close();
+  }
+});
+
+// Обрабатываем отправку формы
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  createBook(title.value, author.value, pages.value, read.checked);
+  dialog.close();
+});
+
+//Функция для просмотра книг в массие
+function searchBook(array) {
+  array.forEach((element) => {
+    console.log(element.title);
+  });
+}
+
+// сброс значений в диалоге
+function reset() {
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  read.checked = false;
+}
